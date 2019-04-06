18964e586c60c5eea726be5b14322cc4ede827ec
Pedro Javier <pedrojavier@iMac-de-Usuario.local>
2019-04-06 10:47:16 +0200

    actualización

--- nodejs/index.js
+++ nodejs/index.js
@@ -6,35 +6,37 @@ const token = '683435743:AAELMu_as_NV_FJDT08IhHldHIYySYjTzac';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+var IdMiChat = 668561478;
+var SerialPort = require('serialport');
+var MiPuerto = new SerialPort('/dev/cu.usbmodemFD141', {
+  baudRate: 9600,
+  autoOpen: true
 });
+
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  //creamos una variable donde guardar la opción que se enviará o apagar/encender
-  var Mensaje = msg.text;
-  if(Mensaje== "encender"){
-    console.log("encender el led")
-      bot.sendMessage(chatId, 'Encenderé el led');
-  }
-  else if (Mensaje == "apagar") {
-    console.log("agapar el led")
-      bot.sendMessage(chatId, 'Apagaré el led');
+        const chatId = msg.chat.id;
+        console.log("El id del chat" + chatId);
+        //creamos una variable donde guardar la opción que se enviará o apagar/encender
+        var Mensaje = msg.text;
+        if(Mensaje== "Hola"){
+            bot.sendMessage(chatId, 'Hola Pedro, Quieres encender la luz o apagar');
+            MiPuerto.write("H");
+        }
+        else if(Mensaje== "encender"){
+            bot.sendMessage(chatId, 'Ahora mismo, encenderé los led');
+            MiPuerto.write("H");
+        }
+        else if (Mensaje == "apagar") {
+            bot.sendMessage(chatId, 'Apagando!!!');
+            MiPuerto.write("L");
 
-  }
+          }
+          else  {
+              bot.sendMessage(chatId, 'No nos entendemos... Quieres encender la luz o apagar ');
 
-  // send a message to the chat acknowledging receipt of their message
+            }
 
 });
